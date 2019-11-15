2bca6861028a614582ea0ff76aab5cfff2689106
Leonardo <leeo.luangg@gmail.com>
2019-11-15 18:47:22 -0300

    adicionando cors ao index

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv/config');
 const express = require('express');
 const routes = require('./src/routes');
+const cors = require('cors');
 
 require('./src/database');
 
@@ -10,6 +11,7 @@ const app = express();
 app.use('/files', express.static('upload', { maxAge: '120 days' }));
 app.use(express.json());
 app.use(routes);
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
