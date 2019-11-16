7dbcf2be7818b4394f7bcfaaf58e2eb72470051d
Leonardo <leeo.luangg@gmail.com>
2019-11-16 12:50:28 -0300

    Finais

--- src/database/seeders/20191110180039-services.js
+++ src/database/seeders/20191110180039-services.js
@@ -4,7 +4,7 @@ module.exports = {
       {
         id: 1,
         user_id: 1,
-        type: 'Tecnologia',
+        ftype: 'Tecnologia',
         description: 'Teste',
         specialization: 'Formatação',
         price_type: 'Por Hora',
